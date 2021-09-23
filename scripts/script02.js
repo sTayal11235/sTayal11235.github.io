b65fd7fedb4cc0f3a294fe0a40916a0535aae17a
sTayal11235 <sarthaktayal15@gmail.com>
2021-09-23 23:35:19 +0530

    look changed

--- scripts/script02.js
+++ scripts/script02.js
@@ -6,18 +6,43 @@ var gmail = document.getElementById("gmail");
 
 
 function display(ele){
-    document.getElementById("cntr").innerHTML = `<img id = "hovimg" src = "${ele.src}"}>`;
+    document.getElementById("cntr").innerHTML = `<img id = "hovimg" src = "${ele.src}" name = "${ele.name}">`;
     centerImgCss();
 }
 
 function Remove() {
-    document.getElementById("hovimg").remove();
+    var img = document.getElementById("hovimg");
+    img.remove();
 }
 
 function centerImgCss(){
-    document.getElementById("hovimg").style.height = "25vw";
-    document.getElementById("hovimg").style.width = "25vw";
-    document.getElementById("hovimg").style.border = "1px solid";
-    document.getElementById("hovimg").style.borderColor = "transparent";
-    document.getElementById("hovimg").style.borderRadius = "50%";
+    var img = document.getElementById("hovimg");
+    img.style.position = "absolute";
+    img.style.top = "7.75%";
+    img.style.left = "7.75%";
+    img.style.height = "21vw";
+    img.style.width = "21vw";
+    img.style.border = "5px";
+    img.style.borderStyle = "solid";
+    img.style.borderRadius = "50%";
+    if(img.name == "git"){
+        img.style.borderColor = "whitesmoke";
+        img.style.boxShadow = "0px 0px 20px 15px rgba(255, 255, 255, 150)";
+    }
+    if(img.name == "cf"){
+        img.style.borderColor = "white";
+        img.style.boxShadow = "0px 0px 30px 15px rgba(170, 235, 255, 150)";
+    }
+    if(img.name == "insta"){
+        img.style.borderColor = "rgb(155, 50, 255)";
+        img.style.boxShadow = "0px 0px 30px 15px rgba(155, 50, 255, 150)";
+    }
+    if(img.name == "lnkdin"){
+        img.style.borderColor = "rgb(0, 120, 255)";
+        img.style.boxShadow = "0px 0px 30px 15px rgba(0, 120, 255, 150)";
+    }
+    if(img.name == "gmail"){
+        img.style.borderColor = "red";
+        img.style.boxShadow = "0px 0px 30px 15px rgba(255, 0, 0, 150)";
+    }
 }
